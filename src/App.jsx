7137355db39659100ab7f5afb3a57dacd05339bb
JerePalmu <jerepalmu03@gmail.com>
2024-12-16 13:46:52 +0200

    Poistettu turhia juttuja

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './App.css'
 import MainView from './MainView';
-import NoteList from './NoteList';
-import NoteSession from './NoteSession';
-import CourseManager from './CourseManager';
 
 function App() {
   return (
