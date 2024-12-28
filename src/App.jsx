a2f3216dd5c7950f97ed9ddd2fd206f12bf5624b
JerePalmu <jerepalmu03@gmail.com>
2024-12-28 15:19:12 +0200

    Notes.jsx -> NoteAdd.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import MainView from './MainView';
 import NoteList from './NoteList';
-import NoteSession from './Notes';
+import NoteSession from './NoteAdd';
 import CourseManager from './CourseManager';
 
 function App() {
